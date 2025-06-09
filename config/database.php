aad843b4423c38d17f427305759a390b152d8fe8
JihanPaperwork <jihanjihan05hum@gmail.com>
2025-06-09 16:32:58 +0700

    update

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@
 $host = "localhost"; // atau alamat IP server database kamu
 $db_name = "scp";
 $username_db = "root"; // username database kamu
-$password_db = "newpassword"; // password database kamu (kosongkan jika tidak ada)
+$password_db = ""; // password database kamu (kosongkan jika tidak ada)
 
 try {
     $conn = new PDO("mysql:host={$host};dbname={$db_name}", $username_db, $password_db);
