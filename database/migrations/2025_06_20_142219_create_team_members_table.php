8b7bd12ac7c656eec48122f81fe4a678e352aa0d
JihanPaperwork <jihanjihan05hum@gmail.com>
2025-06-30 21:21:32 +0700

    update

--- database/migrations/2025_06_20_142219_create_team_members_table.php
+++ database/migrations/2025_06_20_142219_create_team_members_table.php
@@ -6,24 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('team_members', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('student_id'); // Untuk NIM 23.11.xxxx
+            $table->string('student_id');
             $table->string('job_title');
-            $table->string('image_url'); // Akan menyimpan URL gambar dari Cloudinary
+            $table->string('image_url');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('team_members');
--- database/migrations/2025_06_22_085124_create_logs_table.php
+++ database/migrations/2025_06_22_085124_create_logs_table.php
@@ -6,23 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('logs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable()->constrained()->onDelete('set null'); // Siapa yang melakukan
-            $table->string('action');      // Aksi apa? (e.g., 'HAPUS_BACKUP')
-            $table->string('description'); // Deskripsi detail
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('set null');
+            $table->string('action');
+            $table->string('description');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('logs');
