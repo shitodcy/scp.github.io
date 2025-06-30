97047e9e2c71fd9ba5ca03bdfc70800530fc7459
Iruru21 <khaifikk121@gmail.com>
2025-06-30 20:36:02 +0700

    update

--- config/logging.php
+++ config/logging.php
@@ -7,16 +7,7 @@ use Monolog\Processor\PsrLogMessageProcessor;
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Log Channel
-    |--------------------------------------------------------------------------
-    |
-    | This option defines the default log channel that is utilized to write
-    | messages to your logs. The value provided here should match one of
-    | the channels present in the list of "channels" configured below.
-    |
-    */
+    
 
     'default' => env('LOG_CHANNEL', 'stack'),
 
